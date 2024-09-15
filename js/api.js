cf6f12a4117b548f6df253afe9400baea0477947
hlimhli <144866905+h-lim-hl@users.noreply.github.com>
2024-09-15 14:35:44 +0800

    completed  api url map, implement proper rainviewer api call

--- js/api.js
+++ js/api.js
@@ -1,46 +1,100 @@
 const OPEN_DATA_API = {
-"url": {
-  "RTD_24H_Forecast":
-    `https://api-open.data.gov.sg/v2/real-time/api/twenty-four-hr-forecast`,
+  "url": {
+    "RTD_24H_Forecast":
+      `https://api-open.data.gov.sg/v2/real-time/api/twenty-four-hr-forecast`,
 
-  "RT1_RH":
-    `https://api-open.data.gov.sg/v2/real-time/api/relative-humidity`,
-  "RT1_AIR_TEMP":
-    `https://api-open.data.gov.sg/v2/real-time/api/air-temperature`,
-  "RT1_WIND_SPD":
-    `https://api-open.data.gov.sg/v2/real-time/api/wind-speed`,
-  "RT1_WIND_DIR":
-    `https://api-open.data.gov.sg/v2/real-time/api/wind-direction`,
+    "RT1_RH":
+      `https://api-open.data.gov.sg/v2/real-time/api/relative-humidity`,
+    "RT1_AIR_TEMP":
+      `https://api-open.data.gov.sg/v2/real-time/api/air-temperature`,
+    "RT1_WIND_SPD":
+      `https://api-open.data.gov.sg/v2/real-time/api/wind-speed`,
+    "RT1_WIND_DIR":
+      `https://api-open.data.gov.sg/v2/real-time/api/wind-direction`,
 
-  "RT5_RAINFALL": `https://api-open.data.gov.sg/v2/real-time/api/rainfall`,
+    "RT5_RAINFALL": `https://api-open.data.gov.sg/v2/real-time/api/rainfall`,
 
-  "RT60_UVI": `https://api-open.data.gov.sg/v2/real-time/api/uv`,
-  "RT60_PSI": `https://api-open.data.gov.sg/v2/real-time/api/psi`,
-  "RT60_PM25": `https://api-open.data.gov.sg/v2/real-time/api/pm25`,
+    "RT60_UVI": `https://api-open.data.gov.sg/v2/real-time/api/uv`,
+    "RT60_PSI": `https://api-open.data.gov.sg/v2/real-time/api/psi`,
+    "RT60_PM25": `https://api-open.data.gov.sg/v2/real-time/api/pm25`,
 
-  "RT12H_4D_Forecast":
-    `https://api-open.data.gov.sg/v2/real-time/api/four-day-outlook`,
-  "RT12H_2D_Forecast":
-    `https://api-open.data.gov.sg/v2/real-time/api/two-hr-forecast`,
-},
+    "RT12H_4D_Forecast":
+      `https://api-open.data.gov.sg/v2/real-time/api/four-day-outlook`,
+    "RT12H_2D_Forecast":
+      `https://api-open.data.gov.sg/v2/real-time/api/two-hr-forecast`,
+  },
 
   "updateApiDataFns": new Map([
     ["1M", async function () {
+        const endpoints = [
+          OPEN_DATA_API.url.RT1_RH,
+          OPEN_DATA_API.url.RT1_AIR_TEMP,
+          OPEN_DATA_API.url.RT1_WIND_SPD,
+          OPEN_DATA_API.url.RT1_WIND_DIR
+        ];
 
+        await axios.all(endpoints.map((endpoint) => axios.get(endpoint)))
+          .then(axios.spread((relHum, airTemp, windSpd, windDir) => {
+            console.log(relHum);
+            console.log(airTemp);
+            console.log(windSpd);
+            console.log(windDir);
+          }))
+          .catch((err) => {
+            console.error(`updateApiDataFns.1M(): ${err}`);
+          });
+        
     }],
     ["5M", async function () {
+        const endpoints = [
+          OPEN_DATA_API.url.RT5_RAINFALL
+        ];
 
+        await axios.all(endpoints.map((endpoint) => axios.get(endpoint)))
+          .then(axios.spread((rainfall) => {
+            console.log(rainfall);
+          }))
+          .catch((err) => {
+            console.error(`updateApitDataFn.5M(): ${err}`);
+          });
     }],
     ["1H", async function () {
+      const endpoints = [
+        OPEN_DATA_API.url.RT60_PM25,
+        OPEN_DATA_API.url.RT60_PSI,
+        OPEN_DATA_API.url.RT60_UVI
+      ];
 
+      await axios.all(endpoints.map((endpoint) => axios.get(endpoint)))
+        .then(axios.spread((pm25, psi, uvi) => {
+          console.log(pm25);
+          console.log(psi);
+          console.log(uvi);
+        }))
+        .catch((err) => {
+          console.error(`updateApitDataFnMap.5M(): ${err}`);
+        });
     }],
     ["12H", async function () {
-
+      const endpoints = [
+        OPEN_DATA_API.url.RT12H_2D_Forecast,
+        OPEN_DATA_API.url.RT12H_4D_Forecast
+      ];
+      await axios.all(endpoints.map((endpoint) => axios.get(endpoint)))
+        .then(axios.spread((forecast2D, forecast4D) => {
+          console.log(forecast2D);
+          console.log(forecast4D);
+        }))
+        .catch((err) => {
+          console.error(`updateApitDataFnMap.5M(): ${err}`);
+        });
     }]
   ]),
 
   "updateApiDataAll": async function () {
-    for (let updatefn of API.updateApiDataFns.values()) { await updatefn(); }
+    for (let updatefn of OPEN_DATA_API.updateApiDataFns.values()) {
+      await updatefn(); 
+    }
   },
 };
 
@@ -49,38 +103,26 @@ Object.freeze(OPEN_DATA_API);
 const RAIN_VIEWER_API = {
   "apiJson": `https://api.rainviewer.com/public/weather-maps.json`,
   "json": undefined,
-  /*
-  data{
-    generated: 1725246031,
-    host: "https://tilecache.rainviewer.com"
-    radar: {
-      nowcast: [
-        { time: 1725246600, path: "/v2/radar/nowcast_59697c02526b" },,,
-      ],
-      
-      past: [{ time: 1725238800, path: "/v2/radar/1725238800" },,, 
-      ],
-
-      satellite: {
-        infrared: [time: 1725238800, path: "/v2/satellite/c466bd25759e",,,,]
-      }
-    }
-  }
-  */
+  "highRes": 512,
+  "lowRes": 256,
+  "isReady" : false,
 };
-
 async function getRainViewerApiConfig() {
   axios.get(RAIN_VIEWER_API.apiJson)
     .then((response) => {
       console.log(`getRainViewerApiConfig(): Success`);
       console.log(response.data);
       RAIN_VIEWER_API.json = response.data;
+      
+      console.log("rainviewerReady");
+      document.dispatchEvent(rainviewerApiEvent);
+      RAIN_VIEWER_API.isReady = true;
     })
     .catch((err) => {
       console.error(`getRainViewerApiConfig(): ${err}`);
     });
 }
-
+const rainviewerApiEvent = new CustomEvent(`RainviewerApiReady`);
 getRainViewerApiConfig();
 
 const apiData = {
@@ -89,7 +131,12 @@ const apiData = {
     "stations": []
   },
   "uvi": [],
-  "psi": []
+  "psi": [],
+
+
+  "isRainviewerReady" : false,
+
+  "last" : 0
 };
 
 Object.seal(apiData);
--- js/map.js
+++ js/map.js
@@ -3,6 +3,9 @@ const SINGAPORE_LATLONG = [1.3521, 103.8198];
 const ONE_MAP_SG = 0x01;
 const OPEN_STREET_MAP = 0x02;
 
+const MAP_MIN_ZOOM = 11;
+const MAP_MAX_ZOON = 19;
+
 let map = L.map('map').setView(SINGAPORE_LATLONG, 13);
 
 let mapBaseLayer = 2;
@@ -10,25 +13,38 @@ let mapBaseLayer = 2;
 if (Boolean(mapBaseLayer & ONE_MAP_SG)) {
   L.tileLayer('https://www.onemap.gov.sg/maps/tiles/Default_HD/{z}/{x}/{y}.png', {
     detectRetina: true,
-    maxZoom: 19,
-    minZoom: 11,
+    maxZoom: MAP_MAX_ZOON,
+    minZoom: MAP_MIN_ZOOM,
     /** DO NOT REMOVE the OneMap attribution below **/
     attribution: '<img src="https://www.onemap.gov.sg/web-assets/images/logo/om_logo.png" style="height:20px;width:20px;"/>&nbsp;<a href="https://www.onemap.gov.sg/" target="_blank" rel="noopener noreferrer">OneMap</a>&nbsp;&copy;&nbsp;contributors&nbsp;&#124;&nbsp;<a href="https://www.sla.gov.sg/" target="_blank" rel="noopener noreferrer">Singapore Land Authority</a>'
   }).addTo(map);
 }
 if (Boolean(mapBaseLayer & OPEN_STREET_MAP)) {
   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
+    maxZoom: MAP_MAX_ZOON,
+    minZoom: MAP_MIN_ZOOM,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
   }).addTo(map);
 }
 
-
-async function addTitleLayer () {
-  console.log(`${RAIN_VIEWER_API.json.host}${RAIN_VIEWER_API.json.radar.nowcast[0].path}/256/{z}/{x}/{y}/1/1_0.png`);
+async function addTitleLayer() {
+  console.log(
+    `${RAIN_VIEWER_API.json.host}` + 
+    `${RAIN_VIEWER_API.json.radar.nowcast[0].path}` +
+    `/${RAIN_VIEWER_API.highRes}/{z}/{x}/{y}/1/1_0.png`);
   L.tileLayer(
-    `${RAIN_VIEWER_API.json.host}${RAIN_VIEWER_API.json.radar.nowcast[0].path}/256/{z}/{x}/{y}/1/1_0.png`
-  ).addTo(map);
+    `${RAIN_VIEWER_API.json.host}` + 
+    `${RAIN_VIEWER_API.json.radar.nowcast[0].path}` +
+    `/${RAIN_VIEWER_API.highRes}/{z}/{x}/{y}/1/1_0.png`).addTo(map);
 }
 
-setTimeout(addTitleLayer,1000);
+document.addEventListener("RainviewerApiReady", () => {
+  addTitleLayer();
+  console.log("displayRainviewer");
+});
+
+if(RAIN_VIEWER_API.isReady) {
+  addTitleLayer();
+}
+
+OPEN_DATA_API.updateApiDataAll();
